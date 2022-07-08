b2d4144585add9d9f4737eb6b89c855994d7593f
Emily <10801120+emilyblack95@users.noreply.github.com>
2022-07-07 22:43:01 -0700

    Add files via upload

---
+++ GameOfLife/GameState.cs
@@ -0,0 +1,29 @@
+﻿namespace GameOfLife
+{
+    /// <summary>
+    ///  Class for any given state in Conway's Game of Life
+    /// </summary>
+    public class GameState
+    {
+        /// <summary>
+        /// KVP of each Alive Coordinate <--> List of Dead Neighbors on the board
+        /// </summary>
+        public Dictionary<(long, long), HashSet<(long, long)>> Coordinates { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public GameState()
+        {
+            Coordinates = new();
+        }
+
+        /// <summary>
+        /// Overloaded constructor.
+        /// </summary>
+        public GameState(Dictionary<(long, long), HashSet<(long, long)>> coordinates)
+        {
+            Coordinates = coordinates;
+        }
+    }
+}
